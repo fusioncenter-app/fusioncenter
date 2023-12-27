1f6b67d618c3cbb2b87ec1633a7ee50fbc7dfc0a
Mac OS <mac-os@Macs-MacBook-Air.local>
2023-12-27 11:10:50 -0300

    Initial commit 4

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.hashers import make_password  # Import the password hashing function
 from .models import User,Profile
 
+from django.contrib.auth import password_validation
+from django.utils.translation import gettext as _
+
+
 class CustomUserCreationForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
@@ -10,14 +14,22 @@ class CustomUserCreationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name','email',)  # Add other fields as needed
+        fields = ('first_name', 'last_name', 'email',)  # Add other fields as needed
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError(_("Passwords don't match"), code='password_mismatch')
         return password2
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        try:
+            password_validation.validate_password(password1, self.instance)
+        except forms.ValidationError as error:
+            self.add_error('password1', error)
+        return password1
 
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
